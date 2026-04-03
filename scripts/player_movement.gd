447adee805af3323843f190af7eea1c5c0c07c77
tonyrayf <wintryshades@gmail.com>
2026-04-03 23:22:55 +0700

    player movement now is not smoothed (kakashka)

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -2,10 +2,8 @@ extends CharacterBody2D
 
 
 var speed : float = 480
-var last_direction_x : int = 1
-var direction_x : int = last_direction_x
-var last_direction_y : int = 1
-var direction_y : int = last_direction_y
+var last_direction : Vector2 = Vector2(1, 1)
+var direction := last_direction
 var acceleration : float
 var deceleration : float
 const SQRT_2 : float = sqrt(2)
@@ -17,32 +15,14 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	# == Movement X ==
-	direction_x = Input.get_axis("move_left", "move_right")
+	direction.x = Input.get_axis("move_left", "move_right")
 	
-	if direction_x:
-		last_direction_x = direction_x
-		
-		velocity.x += direction_x * acceleration * delta
-	elif sign(velocity.x) == last_direction_x:
-		velocity.x -= last_direction_x * deceleration * delta
-	else:
-		velocity.x = 0
-	
-	velocity.x = clamp(velocity.x, -speed, speed)
+	velocity.x = speed * direction.x
 	
 	# == Movement Y ==
-	direction_y = Input.get_axis("move_up", "move_down")
+	direction.y = Input.get_axis("move_up", "move_down")
 	
-	if direction_y:
-		last_direction_y = direction_y
-		
-		velocity.y += direction_y * acceleration * delta
-	elif sign(velocity.y) == last_direction_y:
-		velocity.y -= last_direction_y * deceleration * delta
-	else:
-		velocity.y = 0
-	
-	velocity.y = clamp(velocity.y, -speed, speed)
+	velocity.y = speed * direction.y
 	
 	if velocity.length() > speed:
 		velocity = velocity.limit_length(speed)
